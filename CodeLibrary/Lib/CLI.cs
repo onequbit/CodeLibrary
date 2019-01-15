39b5f197123aa812bf7d049fb9d12d0572360732
Alfred R Aquino <araquino@developer.com>
2019-01-15 13:03:55 -0500

    - added static classes for invoking Command-Line statements and obtaining the name of the executing process

---
+++ CodeLibrary/Lib/CLI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ServerTools
+{
+    public static class CLI
+    {
+        public static string CallCmd(string argstr)
+        {
+            return Call("cmd", "/c " + argstr);            
+        }
+
+        public static string Call(string cmd, string argstr)
+        {
+            string output = "";
+            using (Process p = new Process())
+            {                
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = cmd;
+                p.StartInfo.Arguments = argstr;
+                p.Start();
+                output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+            }
+            return output;
+        }
+    }
+}
