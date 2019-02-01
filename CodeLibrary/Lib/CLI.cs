76e405eab3e3c7f19a549252b3a74d3290c165cc
Alfred R Aquino <araquino@developer.com>
2019-02-01 16:10:52 -0500

    - Changed from .NET Core to .NET Standard, because I suddenly grew a brain.

--- CodeLibrary/Lib/CLI.cs
+++ CodeLibrary/Lib/CLI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 
-namespace ServerTools
+namespace CodeLibrary
 {
     public static class CLI
     {
--- CodeLibrary/Lib/ThisApp.cs
+++ CodeLibrary/Lib/ThisApp.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ServerTools
+namespace CodeLibrary
 {
     class MySelf
     {
